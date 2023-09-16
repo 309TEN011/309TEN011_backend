12e586bcbd14f504be958ab4d0a7ba61341779fb
syp0812 <94878158+syp0812@users.noreply.github.com>
2023-09-16 23:44:31 +0900

    chore: 프로젝트 기본 환경 설정

---
+++ src/main/java/com/example/potenday/PotendayApplication.java
@@ -0,0 +1,13 @@
+package com.example.potenday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotendayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotendayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/potenday/PotendayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.potenday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotendayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
